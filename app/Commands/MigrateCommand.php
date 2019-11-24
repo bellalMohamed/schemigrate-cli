2a64764d80226f3350070634858039579b9bc37d
Bellal Mohamed <mbellal2000@gmail.com>
2019-11-24 21:15:05 +0200

    removed schedual block

--- app/Commands/MigrateCommand.php
+++ app/Commands/MigrateCommand.php
@@ -85,14 +85,4 @@ class MigrateCommand extends Command
             $this->error("\n{$error}");
         });
     }
-    /**
-     * Define the command's schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
-     * @return void
-     */
-    public function schedule(Schedule $schedule): void
-    {
-        // $schedule->command(static::class)->everyMinute();
-    }
 }
